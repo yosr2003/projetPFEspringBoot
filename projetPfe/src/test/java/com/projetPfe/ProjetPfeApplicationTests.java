42531bb0b400c885a3d73664090153daa8238f11
yosr2003 <yosr.amamou@yahoo.com>
2025-03-18 13:58:06 +0100

    first commit

---
+++ projetPfe/src/test/java/com/projetPfe/ProjetPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetPfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
