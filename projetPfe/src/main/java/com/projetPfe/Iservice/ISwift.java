15b8652d64aa6ab99f0545a3838a353998865cca
khadija404 <chaarkhadija2003@gmail.com>
2025-03-18 16:10:46 +0100

    .....

---
+++ projetPfe/src/main/java/com/projetPfe/Iservice/ISwift.java
@@ -0,0 +1,5 @@
+package com.projetPfe.Iservice;
+
+public interface ISwift {
+
+}
---
+++ projetPfe/src/main/java/com/projetPfe/implService/SwiftServiceImpl.java
@@ -0,0 +1,9 @@
+package com.projetPfe.implService;
+
+import org.springframework.stereotype.Service;
+
+import com.projetPfe.Iservice.ISwift;
+@Service
+public class SwiftServiceImpl implements ISwift{
+
+}
