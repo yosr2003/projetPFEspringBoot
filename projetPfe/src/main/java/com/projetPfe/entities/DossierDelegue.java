01b53b1fe00a3ef734b60bc6289c0d8d66354bd1
khadija404 <chaarkhadija2003@gmail.com>
2025-03-18 15:43:48 +0100

    properties+entities

---
+++ projetPfe/src/main/java/com/projetPfe/entities/DossierDelegue.java
@@ -0,0 +1,112 @@
+package com.projetPfe.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class DossierDelegue {
+	@Id
+	private String idDossier;
+	private String anneedoss;
+	private LocalDateTime  dateCre;
+	private String etatDoss;
+	private LocalDateTime  datclo;
+	private String motifclo;
+	private LocalDate dateDebut;
+	private LocalDate dateExpiration;
+	private Double solde;
+	
+	
+
+	public DossierDelegue() {
+		super();
+	}
+	
+	@OneToMany(mappedBy = "dossierDelegue")
+	private List<Transfert> transferts;
+	
+	
+/// getters and setters
+	public LocalDate getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(LocalDate dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public LocalDate getDateExpiration() {
+		return dateExpiration;
+	}
+
+	public void setDateExpiration(LocalDate dateExpiration) {
+		this.dateExpiration = dateExpiration;
+	}
+
+	public Double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
+	public String getIdDossier() {
+		return idDossier;
+	}
+
+	public void setIdDossier(String idDossier) {
+		this.idDossier = idDossier;
+	}
+
+	public String getAnneedoss() {
+		return anneedoss;
+	}
+
+	public void setAnneedoss(String anneedoss) {
+		this.anneedoss = anneedoss;
+	}
+
+	public LocalDateTime  getDateCre() {
+		return dateCre;
+	}
+
+	public void setDateCre(LocalDateTime  dateCre) {
+		this.dateCre = dateCre;
+	}
+
+	public String getEtatDoss() {
+		return etatDoss;
+	}
+
+	public void setEtatDoss(String etatDoss) {
+		this.etatDoss = etatDoss;
+	}
+
+	public LocalDateTime  getDatclo() {
+		return datclo;
+	}
+
+	public void setDatclo(LocalDateTime  datclo) {
+		this.datclo = datclo;
+	}
+
+	public String getMotifclo() {
+		return motifclo;
+	}
+
+	public void setMotifclo(String motifclo) {
+		this.motifclo = motifclo;
+	}
+/// getters and setters	
+	
+	
+	
+
+}
---
+++ projetPfe/src/main/java/com/projetPfe/entities/Transfert.java
@@ -0,0 +1,115 @@
+package com.projetPfe.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Transfert {
+	@Id
+	private String refTransfert;
+	private Double montantTransfert;
+	private String deviseTransfert;
+	private LocalDateTime  datecre;
+	private EtatDoss etat;
+	private LocalDate dateEcheance;
+	private String natureTransfert;
+	private Double frais;
+
+	public Transfert() {
+		super();
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "idDossDelegue")
+	private DossierDelegue dossierDelegue;
+
+/// getters and setters 
+	public String getRefTransfert() {
+		return refTransfert;
+	}
+
+
+	public void setRefTransfert(String refTransfert) {
+		this.refTransfert = refTransfert;
+	}
+
+
+	public Double getMontantTransfert() {
+		return montantTransfert;
+	}
+
+
+	public void setMontantTransfert(Double montantTransfert) {
+		this.montantTransfert = montantTransfert;
+	}
+
+
+	public String getDeviseTransfert() {
+		return deviseTransfert;
+	}
+
+
+	public void setDeviseTransfert(String deviseTransfert) {
+		this.deviseTransfert = deviseTransfert;
+	}
+
+
+	public LocalDate getDateEcheance() {
+		return dateEcheance;
+	}
+
+
+	public void setDateEcheance(LocalDate dateEcheance) {
+		this.dateEcheance = dateEcheance;
+	}
+
+
+	public String getNatureTransfert() {
+		return natureTransfert;
+	}
+
+
+	public void setNatureTransfert(String natureTransfert) {
+		this.natureTransfert = natureTransfert;
+	}
+
+
+	public Double getFrais() {
+		return frais;
+	}
+
+
+	public void setFrais(Double frais) {
+		this.frais = frais;
+	}
+
+
+	public LocalDateTime  getDatecre() {
+		return datecre;
+	}
+
+
+	public void setDatecre(LocalDateTime  datecre) {
+		this.datecre = datecre;
+	}
+
+
+	public EtatDoss getEtat() {
+		return etat;
+	}
+
+
+	public void setEtat(EtatDoss etat) {
+		this.etat = etat;
+	}
+	
+/// getters and setters 
+
+}
