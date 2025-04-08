7d04b02ee5fa2a9cafd8e70ad8acd19da82d3e0f
Ons-sfaxi <sfaxi.ones@gmail.com>
2025-04-08 14:18:46 +0100

    mod

---
+++ .history/projetPfe/src/main/java/com/projetPfe/controllers/DossierDelegueController_20250408141628.java
@@ -0,0 +1,49 @@
+package com.projetPfe.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.projetPfe.Iservice.IDossierDelegueService;
+import com.projetPfe.entities.DossierDelegue;
+//import com.projetPfe.entities.Response;
+
+
+
+@RestController
+@RequestMapping("/dossiersDelegues")
+public class DossierDelegueController {
+	@Autowired
+	private IDossierDelegueService dossDelService;
+	
+	@GetMapping
+    public ResponseEntity<List<DossierDelegue>> getAllDossiers() {
+        return ResponseEntity.ok(dossDelService.getAllDossierDelegues());
+    }
+	
+	 @GetMapping("/{id}")
+	 public ResponseEntity<DossierDelegue> getDossierById(@PathVariable String id) {
+	        Optional<DossierDelegue> dossier = dossDelService.getDossierById(id);
+	        return dossier.map(ResponseEntity::ok)
+	                .orElseGet(() -> ResponseEntity.notFound().build());
+	    }
+	 
+	 @PutMapping("/{id}")
+	 public ResponseEntity<DossierDelegue> cloturerDossier(@RequestBody DossierDelegue d,@PathVariable("id") String id){
+		 return dossDelService.cloturerDossier(d,id);
+	 }
+	 
+	 
+
+	
+
+}
