5052562fef7a3e0e702b13dff239e837e552c333
Ons-sfaxi <sfaxi.ones@gmail.com>
2025-04-10 08:40:37 +0100

    **

--- projetPfe/projetPfe/src/main/java/com/projetPfe/repositories/TransfertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetPfe.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetPfe.entities.Transfert;
-
-public interface TransfertRepository extends JpaRepository<Transfert, String> {
-
-}
