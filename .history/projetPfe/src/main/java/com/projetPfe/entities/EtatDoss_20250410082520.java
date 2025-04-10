f52faa6198c56794cf66f8d5eb492d6ded20e823
Ons-sfaxi <sfaxi.ones@gmail.com>
2025-04-10 08:26:30 +0100

    .

---
+++ .history/projetPfe/src/main/java/com/projetPfe/entities/EtatDoss_20250410082520.java
@@ -0,0 +1,11 @@
+package com.projetPfe.entities;
+
+public enum EtatDoss {
+	TRAITEMENT,
+	BROUILLON,
+	ANNULE,
+	VALIDE,
+	CLOTURE,
+	ENOVYE,
+	SUSPENDU,
+}
---
+++ .history/projetPfe/src/main/java/com/projetPfe/entities/EtatDoss_20250410082525.java
@@ -0,0 +1,12 @@
+package com.projetPfe.entities;
+
+public enum EtatDoss {
+	TRAITEMENT,
+	BROUILLON,
+	ANNULE,
+	VALIDE,
+	CLOTURE,
+	ENOVYE,
+	SUSPENDU,
+    Prolongé
+}
