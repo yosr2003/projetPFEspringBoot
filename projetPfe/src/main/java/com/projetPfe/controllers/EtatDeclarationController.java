7fe71faae5454ec6ae343a9f7a701772b3745e96
khadija404 <chaarkhadija2003@gmail.com>
2025-04-13 00:29:13 +0100

    ........

--- projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
+++ projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,16 +31,39 @@ public class EtatDeclarationController {
 	
 
 	 
-	 @GetMapping("/test")
-	 public ResponseEntity<byte[]> test(@RequestBody Map<String, String> requestBody) throws Exception{
-		 String typeDeclaration = requestBody.get("typeDeclaration");
-		 String trimestre = requestBody.get("trimestre");
-		 byte[] pdfBytes = etaDecService.test(trimestre,typeDeclaration);
-		 return ResponseEntity.ok()
-	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etat_investissement.pdf")
-	                .contentType(MediaType.APPLICATION_PDF)
-	                .body(pdfBytes);
-	 }
+//	 @GetMapping("/test")
+//	 public ResponseEntity<byte[]> test(@RequestBody Map<String, String> requestBody) throws Exception{
+//		 String typeDeclaration = requestBody.get("typeDeclaration");
+//		 String trimestre = requestBody.get("trimestre");
+//		 byte[] pdfBytes = etaDecService.test(trimestre,typeDeclaration);
+//		 return ResponseEntity.ok()
+//	                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etat_investissement.pdf")
+//	                .contentType(MediaType.APPLICATION_PDF)
+//	                .body(pdfBytes);
+//	 }
+	@GetMapping("/test")
+	public ResponseEntity<?> test(@RequestBody Map<String, String> requestBody) {
+	    
+	        String typeDeclaration = requestBody.get("typeDeclaration");
+	        String trimestre = requestBody.get("trimestre");
+	        byte[] pdfBytes;
+			try {
+				pdfBytes = etaDecService.test(trimestre, typeDeclaration);
+				if(pdfBytes!=null) {
+			        return ResponseEntity.ok()
+			                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=etat_investissement.pdf")
+			                .contentType(MediaType.APPLICATION_PDF)
+			                .body(pdfBytes);
+			    }return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("il n'ya pas de transferts a mettre dans le rapport");
+			} catch (Exception e) {
+				
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(" Une erreur est survenue lors de la génération de l’état de déclaration");
+			}
+	        
+		    	
+		    
+	}
+
 	 
 	 @GetMapping("/test/{id}")
 	 public ResponseEntity<byte[]> genererEtatDeclaration(@RequestBody Map<String, String> requestBody,@PathVariable int id) throws Exception{
