61ecb88cfaae3eba0376bc5a41a55d7cbdda6c46
khadija404 <chaarkhadija2003@gmail.com>
2025-04-14 14:09:19 +0100

    merge

--- projetPfe/src/test/java/com/projetPfe/TransfertTest.java
+++ projetPfe/src/test/java/com/projetPfe/TransfertTest.java
@@ -19,30 +19,7 @@ import com.projetPfe.repositories.TauxChangeRepository;
 import com.projetPfe.repositories.TransfertRepository;
 
 public class TransfertTest {
-	@InjectMocks
-    private TransfertServiceImpl transfertService;
-
-    @Mock
-    private TransfertRepository transfertRepo;
-    @Mock
-    private TauxChangeRepository tauxchangeRepo;
-	 @Test
-	    void testCalculerFrais_TNDSource_BEN() {
-	        TauxChange taux = new TauxChange();
-	        taux.setDevise("EUR");
-	        taux.setCoursVente(3.2);
-
-	        Mockito.when(tauxchangeRepo.findByDevise("EUR")).thenReturn(Optional.of(taux));
-
-	        Optional<Object> result = transfertService.calculerFrais(100.0, "EUR", "TND", "BEN", 20.0);
-
-	        assertTrue(result.isPresent());
-	        Map<String, Object> res = (Map<String, Object>) result.get();
-	        assertEquals(100.0, res.get("montantInitial"));
-	        assertEquals(320.0, res.get("montantConverti"));
-	        assertEquals(300.0, res.get("montantFinal"));
-	        assertEquals(3.2, res.get("TauxChange"));
-	    }
+	
 
 
 }
