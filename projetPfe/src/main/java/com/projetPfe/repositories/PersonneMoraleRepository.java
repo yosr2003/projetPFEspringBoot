842c1285f1cbe3dec43976f9d38fec18d7821ccb
khadija404 <chaarkhadija2003@gmail.com>
2025-04-14 11:33:36 +0100

    repo

---
+++ projetPfe/src/main/java/com/projetPfe/repositories/PersonneMoraleRepository.java
@@ -0,0 +1,5 @@
+package com.projetPfe.repositories;
+
+public interface PersonneMoraleRepository {
+
+}
---
+++ projetPfe/src/main/java/com/projetPfe/repositories/PersonnePhysiqueRepository.java
@@ -0,0 +1,7 @@
+package com.projetPfe.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonnePhysiqueRepository {
+
+}
