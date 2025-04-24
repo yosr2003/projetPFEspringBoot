a69d7b5aa5f8b947f37e85b68654539aadfa00cc
khadija404 <chaarkhadija2003@gmail.com>
2025-04-24 20:12:08 +0100

    etatController

--- projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
+++ projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
@@ -1,5 +1,33 @@
 package com.projetPfe.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetPfe.Iservices.EtatDeclarationIservice;
+@RestController
+@RequestMapping("/etatDeclaration")
 public class EtatDeclarationController {
+	@Autowired
+	private EtatDeclarationIservice etaDecService;
+	
+		@PostMapping("/test")
+	    public ResponseEntity<?> test(@RequestBody Map<String, String> requestBody) {
+	    
+	        String typeDeclaration = requestBody.get("typeDeclaration");
+	        String trimestre = requestBody.get("trimestre");
+		    try {
+				return 	etaDecService.test(trimestre, typeDeclaration);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(" Une erreur est survenue lors de la génération de l’état de déclaration");	
+			}
+	       }
 
 }
