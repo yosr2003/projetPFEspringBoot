69df4310c61e13adea13d5eb8dc88a0ec5b9687b
yosr2003 <yosr.amamou@yahoo.com>
2025-05-25 13:04:11 +0100

    .........................

--- projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
+++ projetPfe/src/main/java/com/projetPfe/controllers/EtatDeclarationController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,8 @@ import com.projetPfe.Iservices.EtatDeclarationIservice;
 public class EtatDeclarationController {
 	@Autowired
 	private EtatDeclarationIservice etaDecService;
-	
+	     
+	 @PreAuthorize("hasRole('BackOffice')")	
 		@PostMapping
 	    public ResponseEntity<?> genererEtatDeclaration(@RequestBody Map<String, String> requestBody) {
 	    
