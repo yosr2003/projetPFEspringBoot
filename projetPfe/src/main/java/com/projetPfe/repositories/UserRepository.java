036e990d670e0320db7ed4f93c0544b100c9139e
yosr2003 <yosr.amamou@yahoo.com>
2025-05-25 11:39:36 +0100

    ............

--- projetPfe/src/main/java/com/projetPfe/repositories/UserRepository.java
+++ projetPfe/src/main/java/com/projetPfe/repositories/UserRepository.java
@@ -1,5 +1,17 @@
 package com.projetPfe.repositories;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetPfe.entities.Employe;
+
+@Repository
+public interface UserRepository extends JpaRepository<Employe, Long> {
+
+Boolean existsByEmail(String email);
+Optional<Employe> findByEmail(String email);
+
 
 }
