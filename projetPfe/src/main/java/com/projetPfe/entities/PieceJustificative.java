bd7a10f488e7376babe84e0f372841302f80987d
yosr2003 <yosr.amamou@yahoo.com>
2025-06-03 20:08:28 +0100

    ..........

---
+++ projetPfe/src/main/java/com/projetPfe/entities/PieceJustificative.java
@@ -0,0 +1,74 @@
+package com.projetPfe.entities;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class PieceJustificative {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomFichier;
+
+    @Lob
+    @JsonIgnore
+    private byte[] contenu;
+
+    @ManyToOne
+    @JsonIgnore
+    private DossierScolarité dossierScolarite; 
+
+    private LocalDateTime dateDepot;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomFichier() {
+		return nomFichier;
+	}
+
+	public void setNomFichier(String nomFichier) {
+		this.nomFichier = nomFichier;
+	}
+
+	public byte[] getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(byte[] contenu) {
+		this.contenu = contenu;
+	}
+
+	public DossierScolarité getDossierSC() {
+		return dossierScolarite;
+	}
+
+	public void setDossierSC(DossierScolarité dossierSC) {
+		this.dossierScolarite = dossierSC;
+	}
+
+	public LocalDateTime getDateDepot() {
+		return dateDepot;
+	}
+
+	public void setDateDepot(LocalDateTime dateDepot) {
+		this.dateDepot = dateDepot;
+	}
+
+      
+}
